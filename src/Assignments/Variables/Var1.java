b401959970b5ef8922ca1b1e2e8eb9294ae5644b
aseelr7 <aseelr95@gmail.com>
2024-08-22 00:24:15 -0400

    first push

---
+++ src/Assignments/Variables/Var1.java
@@ -0,0 +1,14 @@
+package Assignments.Variables;
+
+public class Var1 {
+
+    public static void main(String[] args) {
+
+        int breadCount;
+        int milkLiters;
+        boolean isEggsRequired;
+
+
+
+    }
+}
